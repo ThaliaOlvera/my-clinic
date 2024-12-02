a958c0e459c36d0f704194af8f15464c0995e0be
Thalia Olvera <thalia.olvera.delarosa@gmail.com>
2024-12-02 14:09:10 -0500

    Add: CampoTexto component

---
+++ src/Componentes/CampoTexto/index.js
@@ -0,0 +1,12 @@
+import "./CampoTexto.css";
+
+const CampoTexto = () => {
+  return (
+    <div className="campo-texto">
+      <label>Nombre</label>
+      <input placeholder="Ingresar Nombre" />
+    </div>
+  );
+};
+
+export default CampoTexto;
---
+++ src/Componentes/Header/Header.js
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <section className="Header">
+      <img src="/Img/Header.png" alt="Imagen de Equipo de trabajo" />
+    </section>
+  );
+}
+
+export default Header;
