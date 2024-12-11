01c14253fc98e171a63c4317e576cebc3a0251af
Thalia Olvera <thalia.olvera.delarosa@gmail.com>
2024-12-11 16:45:33 -0500

    Add: Lista Equipo component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from "./Componentes/Header/Header";
 import Formulario from "./Componentes/Formulario/Index";
 import MiOrg from "./Componentes/MiOrg";
+import Equipo from "./Componentes/Equipo";
 
 function App() {
   const [mostrarFormulario, actualizarMostrar] = useState(true);
@@ -10,11 +11,52 @@ function App() {
     actualizarMostrar(!mostrarFormulario);
   };
 
+  const equipos = [
+    {
+      titulo: "Programacion",
+      colorPrimario: "#57C278",
+      colorSecundario: "#D9F7E9",
+    },
+    {
+      titulo: "Front End",
+      colorPrimario: "#82CFFA",
+      colorSecundario: "#E8F8FF",
+    },
+    {
+      titulo: "Data Science",
+      colorPrimario: "#A6D157",
+      colorSecundario: "#F0F8E2",
+    },
+    {
+      titulo: "DevOps",
+      colorPrimario: "#E06B69",
+      colorSecundario: "#FDE7E8",
+    },
+    {
+      titulo: "UX y Diseño",
+      colorPrimario: "#D86EBF",
+      colorSecundario: "#FAE9F5",
+    },
+    {
+      titulo: "Movil",
+      colorPrimario: "#FFBA05",
+      colorSecundario: "#FFF5D9",
+    },
+    {
+      titulo: "Inovacion y Gestion",
+      colorPrimario: "#FF8A29",
+      colorSecundario: "#FFEEDF",
+    },
+  ];
+
   return (
     <div>
       <Header />
       {mostrarFormulario === true ? <Formulario /> : <div></div>}
       <MiOrg cambiarMostrar={cambiarMostrar} />
+      {equipos.map((equipo) => (
+        <Equipo datos={equipo} key={equipo.titulo} />
+      ))}
     </div>
   );
 }
--- src/Componentes/ListaOpciones/index.js
+++ src/Componentes/ListaOpciones/index.js
@@ -1,6 +1,6 @@
 import "./ListaOpciones.css";
 
-const ListaOpciones = () => {
+const ListaOpciones = (props) => {
   const equipos = [
     "Programación",
     "Front End",
@@ -11,12 +11,27 @@ const ListaOpciones = () => {
     "Innovación y Gestión",
   ];
 
+  const manejarCambio = (e) => {
+    console.log("cambio", e.target.value);
+    props.actualizarEquipo(e.target.value);
+  };
+
   return (
     <div className="lista-opciones">
       <label>Equipos</label>
-      <select>
+      <select value={props.valor} onChange={manejarCambio}>
+        <option value="" disabled defaultValue="" hidden>
+          Seleccionar Equipo
+        </option>
+        <option value="" disbled defaultValue="" hidden>
+          Seleccionar Equipo
+        </option>
         {equipos.map((equipo, index) => {
-          return <option key={index}>{equipo}</option>;
+          return (
+            <option key={index} value={equipo}>
+              {equipo}
+            </option>
+          );
         })}
       </select>
     </div>
